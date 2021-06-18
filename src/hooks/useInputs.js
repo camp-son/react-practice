b06a29d3a43b6444b75451ba3695a7d1eb4d99a9
camp-son <sjy2814@gmail.com>
2021-06-18 22:37:54 +0900

    Change useState to useReducer in Custom Hooks

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -1,14 +1,45 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer, useState } from "react";
+
+const EVENT_NAME = {
+	ON_CHANGE: 'ON_CHANGE',
+	ON_RESET: 'ON_RESET'
+};
+
+function reducer(state, action) {
+	switch(action.event) {
+		case EVENT_NAME.ON_CHANGE:
+			return {
+				...state,
+				[action.name]: action.value
+			};
+		case EVENT_NAME.ON_RESET:
+			return {
+				...state,
+				...action.form
+			};
+		default:
+			return state;
+	}
+}
 
 function useInputs(initializeForm) {
-	const [form, setForm] = useState(initializeForm);
+	// const [form, setForm] = useState(initializeForm);
+	const [form, dispatch] = useReducer(reducer, initializeForm);
 
 	const onChange = useCallback((e) => {
 		const {name, value} = e.target;
-		setForm(((form) => ({...form, [name]: value})));
+		// setForm(((form) => ({...form, [name]: value})));
+		dispatch({
+			event: EVENT_NAME.ON_CHANGE,
+			name, 
+			value
+		})
 	}, []);
 
-	const onReset = useCallback(() => setForm(initializeForm), [initializeForm]);
+	const onReset = useCallback(() => dispatch({
+		event: EVENT_NAME.ON_RESET,
+		form: initializeForm
+	}), [initializeForm]);
 
 	return [form, onChange, onReset];
 }
