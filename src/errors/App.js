a51d14c4e56428798363d559b08aa650fc87eb15
camp-son <sjy2814@gmail.com>
2021-06-20 15:38:45 +0900

    componentDidCatch & Sentry

---
+++ src/errors/App.js
@@ -0,0 +1,16 @@
+import ErrorBoundary from './ErrorBoundary';
+import User from './User';
+const App = () => {
+	const user = {
+		id: 1,
+		username: 'camp-son'
+	};
+
+	return (
+		<ErrorBoundary>
+			<User />
+		</ErrorBoundary>
+	);
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Wrapper from './components/01.Wrapper';
+// import Counter from './components/02.Counter';
 import reportWebVitals from './reportWebVitals';
-import Wrapper from './components/01.Wrapper';
-import Counter from './components/02.Counter';
+import App from './errors/App';
+import * as Sentry from '@sentry/browser';
+
+// ReactDOM.render(
+//   	<React.StrictMode>
+// 		<Wrapper>
+// 			<App />
+// 		</Wrapper>
+// 		{/* <Hello /> */}
+// 		<Wrapper>
+// 			<Counter />
+// 		</Wrapper>
+// 		{/* <Wrapper>
+// 			<InputSample />
+// 		</Wrapper> */}
+// 		{/* <Wrapper>
+// 			<Hello 
+// 				name="camp-son"
+// 				color="gray"
+// 			/>
+// 			<Hello 
+// 				name="YJLee"
+// 				color="purple"
+// 				isRequired
+// 			/>
+
+// 			<Hello />
+
+// 			<input/>
+// 			<br/>
+
+// 			<div style={style}>{name}</div>
+// 			<div className="gray-box"></div>
+// 			{divTag}
+// 		</Wrapper> */}
+//   	</React.StrictMode>,
+//   	document.getElementById('root')
+// );
+
+Sentry.init({
+	dsn: 'https://fe4cdd0c5b514e7283a3b718d148e2d1@o873464.ingest.sentry.io/5824894'
+});
 
 ReactDOM.render(
-  	<React.StrictMode>
-		<Wrapper>
-			<App />
-		</Wrapper>
-		{/* <Hello /> */}
-		<Wrapper>
-			<Counter />
-		</Wrapper>
-		{/* <Wrapper>
-			<InputSample />
-		</Wrapper> */}
-		{/* <Wrapper>
-			<Hello 
-				name="camp-son"
-				color="gray"
-			/>
-			<Hello 
-				name="YJLee"
-				color="purple"
-				isRequired
-			/>
-
-			<Hello />
-
-			<input/>
-			<br/>
-
-			<div style={style}>{name}</div>
-			<div className="gray-box"></div>
-			{divTag}
-		</Wrapper> */}
-  	</React.StrictMode>,
-  	document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
