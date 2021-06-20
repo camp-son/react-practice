f9b5a38ddf8a9f13a73fef7c66b25e62ed888f63
camp-son <sjy2814@gmail.com>
2021-06-20 19:05:51 +0900

    TodoTemplate 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.scss';
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background: #e9ecef;
+    }
+`;
 
 function App() {
-    return <div>Hello world</div>;
+    return (
+        <>
+            <GlobalStyle />
+            <TodoTemplate>Hello world</TodoTemplate>
+        </>
+    );
 }
 
 export default App;
