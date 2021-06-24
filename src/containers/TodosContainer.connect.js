8d6b7c41ef9f706c7b273cecfd6e314b6deea4d1
camp-son <sjy2814@gmail.com>
2021-06-24 23:19:02 +0900

    connect 함수 사용 예시

--- src/containers/TodosContainer.connect.js
+++ src/containers/TodosContainer.connect.js
@@ -1,15 +1,8 @@
-import {useCallback} from 'react';
-import {connect, useDispatch, useSelector} from 'react-redux';
+import {connect} from 'react-redux';
 import {addTodo, toggleTodo} from '../modules/todos';
 import Todos from '../components/TodoPresentation/Todos';
 
 const TodosContainer = ({todos, onCreate, onToggle}) => {
-    // const todos = useSelector((state) => state.todos);
-    // const dispatch = useDispatch();
-
-    // const onCreate = (text) => dispatch(addTodo(text));
-    // const onToggle = useCallback((id) => dispatch(toggleTodo(id)), [dispatch]);
-
     return <Todos todos={todos} onCreate={onCreate} onToggle={onToggle} />;
 };
 
