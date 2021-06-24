8b206bef96754c15ed8d7291176efbbb1492a4eb
camp-son <sjy2814@gmail.com>
2021-06-24 22:36:40 +0900

    useSelector 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,12 +1,24 @@
-import {useDispatch, useSelector} from 'react-redux';
+import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import Counter from '../components/Counter/Counter';
 import {decrease, increase, setDiff} from '../modules/counter';
 
 const CounterContainer = () => {
-    const {number, diff} = useSelector((state) => ({
-        number: state.counter.count,
-        diff: state.counter.diff,
-    }));
+    // const {number, diff} = useSelector((state) => ({
+    //     number: state.counter.count,
+    //     diff: state.counter.diff,
+    // }));
+    const {count: number, diff} = useSelector((state) => state.counter);
+    // 1. useSelector 최적화: 객체가 아닌 항목별로 사용한다.
+    // const number = useSelector((state) => state.counter.count);
+    // const diff = useSelector((state) => state.counter.diff);
+    // 2. shallowEqual을 사용한다.
+    // const {number, diff} = useSelector(
+    //     (state) => ({
+    //         number: state.counter.count,
+    //         diff: state.counter.diff,
+    //     }),
+    //     shallowEqual
+    // );
 
     const dispatch = useDispatch();
     const onIncrease = () => dispatch(increase());
